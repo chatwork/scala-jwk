92b034d42ab77affc4254846a9d3ec79e4d75fa3
Scala Steward <me@scala-steward.org>
2020-05-10 12:51:55 +0200

    Update scalatest to 3.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object circe {
 }
 
 object scalatest {
-  val scalatest = "org.scalatest" %% "scalatest" % "3.1.1"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.1.2"
 }
 
 object beachape {
