94c3693b5c66b5b29f004048c2f357355a7d5f82
cw-scala-steward <develop-hq+scala-steward@chatwork.com>
2021-10-15 07:04:21 +0000

    build: Update base64scala from 1.0.29 to 1.0.30

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,5 +27,5 @@ object scalacheck {
 }
 
 object j5ik2o {
-  val base64scala = "com.github.j5ik2o" %% "base64scala" % "1.0.29"
+  val base64scala = "com.github.j5ik2o" %% "base64scala" % "1.0.30"
 }
